d9534604527e96f83c0ac78c529bade3fce64c56
Mariq12 <fmlabanda@utpl.edu.ec>
2024-06-18 20:14:36 -0500

    Correción de description y del borde y color

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,15 +1,29 @@
 import PropTypes from 'prop-types';
 import styles from "./Banner.module.css";
+import categoryData from "../../data/CategoryData"; 
+
+const categoryLookup = categoryData.reduce((acc, category) => {
+    acc[category.name] = category;
+    return acc;
+}, {});
 
 function Banner({ card }) {
+    const { name, primaryColor } = categoryLookup[card.team];
+
+    const titleStyle = {
+        borderColor: primaryColor,
+        color: primaryColor,
+    };
+
     return (
         <main className={styles.layer} style={{ backgroundImage: `url(${card.photo})` }}>
             <div className={styles.gradient}></div>
             <section className={styles.content}>
-                <h1 className={styles.title}>{card.title}</h1>
+                <h1 className={styles.name} style={titleStyle}>{name}</h1>
+                <h2 className={styles.title}>{card.title}</h2>
                 <p className={styles.subtitle}>{card.description}</p>
             </section>
-            <section className={styles.container}>
+            <section className={styles.container} style={{ borderColor: primaryColor, borderStyle: 'solid', borderWidth: '2px' }}>
                 <iframe
                     src={card.link}
                     title={card.title}
@@ -29,6 +43,7 @@ Banner.propTypes = {
         title: PropTypes.string.isRequired,
         link: PropTypes.string.isRequired,
         description: PropTypes.string.isRequired,
+        team: PropTypes.string.isRequired,
     }).isRequired,
 };
 
