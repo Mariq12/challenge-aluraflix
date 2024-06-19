11f4b0adb5ef023164f18d0d506525160ce3f12b
Mariq12 <fmlabanda@utpl.edu.ec>
2024-06-19 13:00:37 -0500

    Se agregó funcionalidad al icono menu

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,26 +1,33 @@
-import { Link } from "react-router-dom"
-import styles from "./Header.module.css"
-import logo from "../../assets/logo.png"
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import styles from "./Header.module.css";
+import logo from "../../assets/logo.png";
 import HeaderLink from "../headerLink/HeaderLink";
 import { IoMenu } from "react-icons/io5";
 
-function Header(){
-    return(
+function Header() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
+    return (
         <header className={styles.header}>
             <Link to="/">
                 <section className={styles.logoContainer}>
-                    <img src={logo} alt="Logo Alura"/>
+                    <img src={logo} alt="Logo Alura" />
                 </section>
             </Link>
             <nav className={styles.nav}>
-                <div className={styles.navLinks}>
+            <IoMenu className={styles.menuIcon} onClick={toggleMenu} />
+                <div className={`${styles.navLinks} ${menuOpen ? styles.showMenu : ""}`}>
                     <HeaderLink url="./">INICIO</HeaderLink>
                     <HeaderLink url="./newVideo">NUEVO VIDEO</HeaderLink>
                 </div>
-                <IoMenu className={styles.menuIcon} />
             </nav>
         </header>
-    )
+    );
 }
 
 export default Header;
